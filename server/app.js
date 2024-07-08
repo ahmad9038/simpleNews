7a4f84b305d0d76fc0c93a9f613a41ace669758f
ahmadShahzad <128154734+ahmad9038@users.noreply.github.com>
2024-07-08 23:10:19 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,15 +3,16 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// const corsOptions = {
-//   origin: 'https://simple-news-client.vercel.app', // or your specific origin
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true,
-//   optionsSuccessStatus: 204
-// };
+const corsOptions = {
+  origin: 'https://simple-news-client.vercel.app', // or your specific origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+};
 
+app.use(cors(corsOptions));
 // app.options('*', cors());
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 require("dotenv").config();
